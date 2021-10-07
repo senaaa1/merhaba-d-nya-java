919f0d3e108fde5bb3d22d8b2a7019bbdfaf85e1
senaaa1 <88001535+senaaa1@users.noreply.github.com>
2021-10-07 11:50:41 +0300

    Add files via upload

---
+++ isimsoyisim.java
@@ -0,0 +1,7 @@
+public class isimsoyisim {
+    public static void main(String[] args) {
+        System.out.println("yas=19");
+        System.out.println("isim=Sena");
+         System.out.println("soyisim=AŞIK");
+        }
+}
---
+++ ortalama.java
@@ -0,0 +1,10 @@
+public class ortalama {
+    public static void main(String[] args){
+        int sayi1=10;
+        int sayi2=20;
+
+        int toplam= sayi1+sayi2;
+        double ortalama=toplam/2.0;
+        System.out.println("bu 2 sayinin ortalaması:"+ortalama);
+    }    
+}
